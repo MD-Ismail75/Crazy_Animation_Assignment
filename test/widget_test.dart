de444df4baa3dc8a8598e4aaa6a4519c912eb667
MD-Ismail75 <c211035@ugrad.iiuc.ac.bd>
2024-11-01 22:22:00 +0600

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:iiuc_student_portal/main.dart';
 
-import 'package:iiuc_student_portal/pages/main.dart';
+//import 'package:iiuc_student_portal/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
